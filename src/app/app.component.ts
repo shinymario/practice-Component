db9618f09aefc49dda5ddd1be06eebb19efe476c
shinymario <shinymario123@gmail.com>
2025-02-26 11:49:25 -0600

    Class activity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CourseCardComponent } from './course-card/course-card.component';
+import { COURSES } from './data/course-data';
+import { Course } from './model/course';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'practice-Component';
+
+  csci160: Course = COURSES[0];
+  csci161: Course = COURSES[1];
+  csci213: Course = COURSES[2];
 }
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-course-card',
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+  @Input() course!: Course;
+}
